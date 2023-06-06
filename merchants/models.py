41ce596491d482bdb0f391753c09e525fc1c71c1
FoxBeCoding <fox@foxbecoding.com>
2023-06-06 15:18:27 -0500

    merchants: created models

--- merchants/models.py
+++ merchants/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Merchant(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="merchant")
+    uid = models.CharField(max_length=20, blank=False, unique=True)
+    title = models.CharField(max_length=500, blank=False )
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantPlan(models.Model):
+    uid = models.CharField(max_length=20, blank=False, unique=True)
+    title = models.CharField(blank=False)
+    description = models.CharField(blank=False)
+    merchant_plan_listings = models.IntegerField(blank=False)
+    is_active = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantPlanPrice(models.Model):
+    merchant_plan = models.ForeignKey(MerchantPlan, on_delete=models.CASCADE, related_name="plan_prices")
+    uid = models.CharField(max_length=20, blank=False, unique=True)
+    title = models.CharField(blank=False)
+    description = models.CharField(blank=False)
+    price = models.FloatField(blank=False)
+    stripe_price_key = models.CharField(blank=False)
+    is_active = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantPlanFeature(models.Model):
+    merchant_plan = models.ForeignKey(MerchantPlan, on_delete=models.CASCADE, related_name="features")
+    title = models.CharField(blank=False)
+    is_active = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantSubcription(models.Model):
+    merchant = models.OneToOneField(Merchant, on_delete=models.CASCADE, related_name="subscription")
+    merchant_plan = models.ForeignKey(MerchantPlan, on_delete=models.CASCADE, related_name="subscribers")
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantStore(models.Model):
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE, related_name="stores")
+    uid = models.CharField(max_length=20, blank=False, unique=True)
+    name = models.CharField(max_length=500, blank=False)
+    description = models.CharField(max_length=2000, blank=False)
+    logo = models.ImageField(upload_to="images/merchant_store_media/logos/", blank=True, null=True)
+    banner_image = models.ImageField(upload_to="images/merchant_store_media/banners/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantStoreView(models.Model):
+    merchant_store = models.ForeignKey(MerchantStore, on_delete=models.CASCADE, related_name="views")
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantStoreDepartment(models.Model):
+    merchant_store = models.ForeignKey(MerchantStore, on_delete=models.CASCADE, related_name="departments")
+    title = models.CharField(max_length=100, blank=False)
+    description = models.CharField(max_length=1000, blank=True, null=True)
+    banner_image = models.ImageField(upload_to="images/merchant_store_media/departments/banners/", blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
+
+class MerchantStoreDepartmentView(models.Model):
+    merchant_store_department = models.ForeignKey(MerchantStoreDepartment, on_delete=models.CASCADE, related_name="views")
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now_add=True, null=True)
+    deleted = models.DateTimeField(null=True)
