7de3d83c9a60deda8ce786030de80914df43d6b1
FoxBeCoding <fox@foxbecoding.com>
2023-06-17 19:09:44 -0500

    users: updated serializers

--- users/serializers.py
+++ users/serializers.py
@@ -242,6 +242,36 @@ class UserAddressSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAddress
         fields = [
+            'pk',
+            'full_name',
+            'phone_number',
+            'street_address',
+            'street_address_ext',
+            'country',
+            'state',
+            'city',
+            'postal_code'
+        ]
+
+class CreateUserAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAddress
+        fields = [
+            'user',
+            'full_name',
+            'phone_number',
+            'street_address',
+            'street_address_ext',
+            'country',
+            'state',
+            'city',
+            'postal_code'
+        ]
+
+class EditUserAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserAddress
+        fields = [
             'full_name',
             'phone_number',
             'street_address',
