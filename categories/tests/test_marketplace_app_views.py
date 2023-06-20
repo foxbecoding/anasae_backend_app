67a90e5796246057ad094010f7f07b298586647e
FoxBeCoding <fox@foxbecoding.com>
2023-06-20 12:02:41 -0500

    categories: updated views

--- categories/tests/test_marketplace_app_views.py
+++ categories/tests/test_marketplace_app_views.py
@@ -10,17 +10,14 @@ class TestMPAUserViewSet(TestCase):
         self.client = Client()
         categories = [
             {
-                'uid': create_uid('cat-'),
                 'title': "Women's Fashion",
                 'description': "Best black women's fashion trends"
             },
             {
-                'uid': create_uid('cat-'),
                 'title': "Men's Fashion",
                 'description': "Best black men's fashion trends"
             },
             {
-                'uid': create_uid('cat-'),
                 'title': "Bags & Shoes",
                 'description': "Best bags & shoes by black designers"
             },
@@ -29,7 +26,6 @@ class TestMPAUserViewSet(TestCase):
         Category_Instances = []
         for cat in categories:
             Category_Instance = Category.objects.create(
-                uid = cat['uid'],
                 title = cat['title'],
                 description = cat['description']
             )
@@ -40,17 +36,14 @@ class TestMPAUserViewSet(TestCase):
         Category_Instances = [ {'category': CI } for CI in Category_Instances ]
         subcategories = [
             {
-                'uid': create_uid('scat-'),
                 'title': "Bottoms",
                 'description': "Shop women's pants, leggings, shorts and more."
             },
             {
-                'uid': create_uid('scat-'),
                 'title': "Bottoms",
                 'description': "Shop men's pants, jeans, shorts and more."
             },
             {
-                'uid': create_uid('scat-'),
                 'title': "Women's Shoes",
                 'description': "Shop top black brands for women shoes"
             }, 
@@ -63,7 +56,6 @@ class TestMPAUserViewSet(TestCase):
             category = scat[1]['category']
             Subcategory_Instance = Subcategory.objects.create(
                 category = category,
-                uid = data['uid'],
                 title = data['title'],
                 description = data['description']
             )
@@ -74,17 +66,14 @@ class TestMPAUserViewSet(TestCase):
         Subcategory_Instances = [ {'subcategory': SCI } for SCI in Subcategory_Instances ]
         subcategory_sections = [
             {
-                'uid': create_uid('scats-'),
                 'title': "Leggings",
                 'description': "Shop women's leggings."
             },
             {
-                'uid': create_uid('scats-'),
                 'title': "Bottoms",
                 'description': "Shop men's jeans."
             },
             {
-                'uid': create_uid('scats-'),
                 'title': "Women's Shoes",
                 'description': "Shop women's flats"
             }, 
@@ -97,7 +86,6 @@ class TestMPAUserViewSet(TestCase):
             subcategory = scats[1]['subcategory']
             Subcategory_Section_Instance = SubcategorySection.objects.create(
                 subcategory = subcategory,
-                uid = data['uid'],
                 title = data['title'],
                 description = data['description']
             )
