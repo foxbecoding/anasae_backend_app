b3a60b83fdc25d4248c714ce520ee303a7571e9e
FoxBeCoding <fox@foxbecoding.com>
2023-06-21 13:27:58 -0500

    merchants: updated unit test

--- merchants/tests/test_merchant_center_app_views.py
+++ merchants/tests/test_merchant_center_app_views.py
@@ -66,5 +66,49 @@ class TestMCMerchantViewSet(TestCase):
         )
         self.assertEqual(res.status_code, 400)
 
+class TestMCMerchantSubscriptionViewSet(TestCase):
+    
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+        date_time_str = '12/31/1990'
+        self.date_time_obj = datetime.strptime(date_time_str, '%m/%d/%Y')
+        self.client.get(reverse('x-fct-list'))
+        self.csrftoken = self.client.cookies['csrftoken'].value
+        self.user_gender = UserGender.objects.create(gender = 'male')
+        self.user_gender.save()
+
+        #User Sign up
+        user_sign_up_data = {
+            'first_name': "Desmond",
+            'last_name': 'Fox',
+            'email': 'fox@foxbecoding.com',
+            'password': '123456',
+            'confirm_password': '123456',
+            'date_of_birth': self.date_time_obj.date(),
+            'agreed_to_toa': True,
+            'gender': self.user_gender.pk
+        }
+
+        self.client.post(
+            reverse('account-sign-up-list'), 
+            user_sign_up_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+
+        #User Login
+        login_credentials = {
+            'email': 'fox@foxbecoding.com',
+            'password': '123456'
+        }
+    
+        #Get response data
+        res = self.client.post(
+            reverse('account-log-in-list'), 
+            login_credentials, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        self.user = res.data
+        self.csrftoken = self.client.cookies['csrftoken'].value
+
     def test_mc_merchant_subscription_create(self):
         pass
