dea1099121f9e0d4511bb0da05286453930d335e
FoxBeCoding <fox@foxbecoding.com>
2023-06-22 14:25:02 -0500

    users: updated permissions

--- users/permissions.py
+++ users/permissions.py
@@ -15,7 +15,7 @@ class UserProfilePermission(BasePermission):
     message = "Access Denied!"   
 
     def has_permission(self, request, view):
-        SAFE_METHODS = ['POST', 'PUT', 'DELETE']
+        SAFE_METHODS = ('POST', 'PUT', 'DELETE')
         if request.method in SAFE_METHODS:
             return True
         return False
@@ -26,13 +26,13 @@ class UserProfilePermission(BasePermission):
         
         if request.method == 'DELETE':
             User_Profile_Instances = UserProfile.objects.filter(pk__in=User_Serializer.data['profiles']).filter(is_account_holder=False)
-            user_profile_pks = [ str(upi.id) for upi in User_Profile_Instances ]  
+            user_profile_pks = (str(upi.id) for upi in User_Profile_Instances)
             
             if str(profile_pk) not in user_profile_pks:
                 return False
             return True
             
-        user_profile_pks = [str(profile) for profile in User_Serializer.data['profiles']] 
+        user_profile_pks = (str(profile) for profile in User_Serializer.data['profiles']) 
         if profile_pk not in user_profile_pks:
             return False
         return True
@@ -42,9 +42,27 @@ class UserProfileImagePermission(BasePermission):
 
     def has_permission(self, request, view):
         User_Serializer = UserSerializer(request.user) 
-        user_profile_pks = [ str(profile) for profile in User_Serializer.data['profiles'] ]  
+        user_profile_pks = (str(profile) for profile in User_Serializer.data['profiles'])  
         
         if str(request.data['user_profile']) not in user_profile_pks:
             return False
         
+        return True
+
+class UserAddressPermission(BasePermission):
+    message = "Access Denied!"   
+
+    def has_permission(self, request, view):
+        SAFE_METHODS = ('POST', 'PUT', 'DELETE')
+        if request.method in SAFE_METHODS:
+            return True
+        return False
+    
+    def has_object_permission(self, request, view, obj):
+        User_Serializer = UserSerializer(request.user) 
+        user_address_pks = (str(address) for address in User_Serializer.data['addresses'])
+
+        if str(obj['address_pk']) not in user_address_pks:
+            return False
+
         return True
