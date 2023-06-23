c274281e69e00c5489a54eac2d161dbe25181a89
FoxBeCoding <fox@foxbecoding.com>
2023-06-23 14:39:52 -0500

    updated unit test

--- merchants/tests/test_merchant_center_app_views.py
+++ merchants/tests/test_merchant_center_app_views.py
@@ -90,6 +90,66 @@ class TestMCMerchantViewSet(TestCase):
         )
         self.assertEqual(res.status_code, 403)
 
+class TestMCMerchantPaymentMethodViewSet(TestCase):
+
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
+        date_time_str = '12/31/1990'
+        self.date_time_obj = datetime.strptime(date_time_str, '%m/%d/%Y')
+        self.client.get(reverse('x-fct-list'))
+        self.csrftoken = self.client.cookies['csrftoken'].value
+        self.user_gender = UserGender.objects.create(gender = 'male')
+        self.user_gender.save()
+
+        #User Sign up
+        user_sign_up_data = {
+            'first_name': "Desmond",
+            'last_name': 'Fox',
+            'email': 'fox@foxbecoding.com',
+            'password': '123456',
+            'confirm_password': '123456',
+            'date_of_birth': self.date_time_obj.date(),
+            'agreed_to_toa': True,
+            'gender': self.user_gender.pk
+        }
+
+        self.client.post(
+            reverse('account-sign-up-list'), 
+            user_sign_up_data, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+
+        #User Login
+        login_credentials = {
+            'email': 'fox@foxbecoding.com',
+            'password': '123456'
+        }
+    
+        #Get response data
+        res = self.client.post(
+            reverse('account-log-in-list'), 
+            login_credentials, 
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        self.user = res.data
+        self.csrftoken = self.client.cookies['csrftoken'].value
+
+        # Create Merchant Account
+        self.merchant_res = self.client.post(
+            reverse('mc-merchant-list'),
+            data = {'title': 'Fenty Beauty'},
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+
+    def test_mc_merchant_payment_method_create(self):
+        res = self.client.post(
+            reverse('mc-merchant-payment-method-list'),
+            data = {},
+            **{'HTTP_X_CSRFTOKEN': self.csrftoken}
+        )
+        self.assertEqual(res.status_code, 201)
+
+
 class TestMCMerchantSubscriptionViewSet(TestCase):
     
     def setUp(self):
