930e4a1ecf04343d3c9d1e99813e214e5cdc5af3
FoxBeCoding <fox@foxbecoding.com>
2023-06-26 12:40:47 -0500

    updated views

--- merchants/ecosystem/merchant_center_app/views.py
+++ merchants/ecosystem/merchant_center_app/views.py
@@ -83,6 +83,7 @@ class MCMerchantSubcriptionViewSet(viewsets.ViewSet):
     
     @method_decorator(csrf_protect)
     def create(self, request):
+        self.check_object_permissions(request=request, obj={})
         print(request.data)
         # stripe.PaymentIntent.create(
         #     amount=2000,
