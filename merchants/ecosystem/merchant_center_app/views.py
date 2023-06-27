16a227110b4caa651b93d0792413ff7c3adcf547
FoxBeCoding <fox@foxbecoding.com>
2023-06-27 14:37:17 -0500

    updated views

--- merchants/ecosystem/merchant_center_app/views.py
+++ merchants/ecosystem/merchant_center_app/views.py
@@ -53,6 +53,11 @@ class MCMerchantPaymentMethodViewSet(viewsets.ViewSet):
 
         Merchant_Instance = Merchant.objects.get(user_id=str(request.user.id))
         payment_method_res = stripe.PaymentMethod.retrieve(id=request.data['payment_method_id'])
+        
+        Stripe_Payment_Method = stripe.PaymentMethod.attach(
+            request.data['payment_method_id'],
+            customer=request.user.stripe_customer_id,
+        )
 
         data = {
             'merchant': Merchant_Instance.id,
@@ -85,14 +90,31 @@ class MCMerchantSubcriptionViewSet(viewsets.ViewSet):
     @method_decorator(csrf_protect)
     def create(self, request):
         self.check_object_permissions(request=request, obj={})
-        stripe_price = stripe.Price.retrieve(request.data['price_key'])
-        print(stripe_price)
-        stripe.PaymentIntent.create(
-            amount=stripe_price.unit_amount,
+        Stripe_Price = stripe.Price.retrieve(request.data['price_key'])
+
+        Stripe_Payment_Intent = stripe.PaymentIntent.create(
+            amount=Stripe_Price.unit_amount,
             currency="usd",
+            customer=request.user.stripe_customer_id,
             automatic_payment_methods={"enabled": True},
-            payment_method=request.data['payment_method'],
+            payment_method=request.data['payment_method']
         )
+
+        Stripe_Payment_Intent = stripe.PaymentIntent.confirm(
+            Stripe_Payment_Intent.id,
+            payment_method = request.data['payment_method'],
+            return_url = 'http://127.0.0.1:3001'
+        )
+
+        # Stripe_Subscription = stripe.Subscription.create(
+        #     customer=request.user.stripe_customer_id,
+        #     items=[
+        #         {"price": request.data['price_key']},
+        #     ],
+        # )
+
+        # print(Stripe_Payment_Intent)
+        # print(Stripe_Subscription)
         return Response(None, status=status.HTTP_201_CREATED)
     
     def retrieve(self, request, pk=None):
